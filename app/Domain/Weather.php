91eb49907ba318d4f1e65a9181f32814ff75fcb8
rn-santos227 <rn_santos227@yahoo.com>
2025-12-29 17:09:15 +0800

    created weather object file

---
+++ app/Domain/Weather.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Domain;
+
+use Carbon\CarbonImmutable;
+
+final class Weather
+{
+    public function __construct(
+        public readonly string $city,
+        public readonly float $temperature,
+        public readonly string $description,
+        public readonly CarbonImmutable $timestamp
+    ) {}
+
+    public function toArray(string $source): array
+    {
+        return [
+            'city' => $this->city,
+            'temperature' => $this->temperature,
+            'weather_description' => $this->description,
+            'timestamp' => $this->timestamp->toIso8601String(),
+            'source' => $source,
+        ];
+    }
+}
