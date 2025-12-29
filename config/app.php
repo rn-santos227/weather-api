7823e6c0636c216369b6312d94bbc5a0af44cf8f
rn-santos227 <rn_santos227@yahoo.com>
2025-12-29 17:03:06 +0800

    added openweather configuration in app config and created api route

--- config/app.php
+++ config/app.php
@@ -123,4 +123,16 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | OpenWeather Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'openweather' => [
+        'key' => env('OPENWEATHER_API_KEY'),
+        'base_url' => env('OPENWEATHER_BASE_URL', 'https://api.openweathermap.org/data/2.5'),
+        'units' => env('OPENWEATHER_UNITS', 'metric'),
+        'timeout' => 5,
+    ],
 ];
